ec22faf2c6e5d6f8e6442fcfce9ba6f78a3933f6
Esqape <44909141+Esqape@users.noreply.github.com>
2022-11-03 05:46:32 +0530

    Removing sensitive firebase credetials

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@
 import * as firebase from "firebase";
 
 const config = {
-    apiKey: "AIzaSyDlS_1q4nmlnJAy8-HQvB6v9G1b0creXmk",
-    authDomain: "certificates-dapp.firebaseapp.com",
-    databaseURL: "https://certificates-dapp.firebaseio.com",
+    apiKey: "xxxx",
+    authDomain: "xxx",
+    databaseURL: "xx",
     projectId: "certificates-dapp",
     storageBucket: "certificates-dapp.appspot.com",
-    messagingSenderId: "1023046526288",
-    appId: "1:1023046526288:web:d3fc0339fd63096e"
+    messagingSenderId: "xxx",
+    appId: "xxx"
 };
 
 export default !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
